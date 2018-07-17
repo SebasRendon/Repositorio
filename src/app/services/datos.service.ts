72155aa4ac43fd4d01df5adcffa33ef09d80bfe7
Jhoan sebastian <jhoansebastianrendon@hotmail.com>
2018-07-17 08:45:28 -0500

    mejoras en la logica al mostrar los datos del servicio

--- src/app/services/datos.service.ts
+++ src/app/services/datos.service.ts
@@ -6,13 +6,15 @@ import { Http } from '@angular/http';
 })
 export class DatosService {
 datos:any[]=[];
+cargando:boolean;
   constructor(private http:Http) {
 console.log("prueba jhoan");
-
+this.cargando=true;
 this.http.get("https://pagina-web-e87a5.firebaseio.com/datos_personales.json")
 .subscribe(respuesta => {
   console.log(respuesta.json());
   this.datos=respuesta.json();
+  this.cargando=false;
 });
 
    }
