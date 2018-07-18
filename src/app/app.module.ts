61a49d0857b3f63bb822e819b339871ffd9b5d2f
Jhoan sebastian <jhoansebastianrendon@hotmail.com>
2018-07-18 10:51:44 -0500

    se agrega imagen y texto al index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 //servicios
 import {DatosService} from "./services/datos.service";
+//rutas
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { HeaderComponent } from './component/header/header.component';
 import { BodyComponent } from './component/body/body.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { IndexComponent } from './component/index/index.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +19,15 @@ import { IndexComponent } from './component/index/index.component';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-    IndexComponent
+    IndexComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
  HttpModule
+
+
+
   ],
   providers: [  DatosService],
   bootstrap: [AppComponent]
---
+++ src/app/component/educacion/educacion.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-educacion',
+  templateUrl: './educacion.component.html',
+
+})
+export class EducacionComponent {
+
+  constructor() { }
+
+
+
+}
