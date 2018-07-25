36efc5c4b57498e4be82d1312355fd83b9ec5b52
Jhoan Rendon <jhoansebastianrendon2013@gmail.com>
2018-07-24 21:58:21 -0500

    mejoras en las rutasmejoras en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,12 +13,14 @@ IndexComponent,
 
 
 const app_routes: Routes = [
-  { path: 'index', component: IndexComponent },
+  { path: 'Index', component: IndexComponent },
   { path: 'educacion', component: EducacionComponent },
   { path: 'body', component: BodyComponent },
   { path: 'experiencia', component: ExperienciaComponent },
   { path: 'item/:id', component: PortafolioItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'index' }
+  { path: '**', pathMatch: 'full', redirectTo: 'Index' }
 ];
 
-export const app_routing = RouterModule.forRoot(app_routes);
+export const app_routing = RouterModule.forRoot(app_routes,{ useHash:true});
+//el useHash va crean rutas que no sean de html5
+// es utilizado para aunmentar la compactinblidad cuando despleguemos el sitio
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent  {
   constructor(public _ds:DatosService,
                public router:Router) {
 
-  }
+               }
 
 
 }
